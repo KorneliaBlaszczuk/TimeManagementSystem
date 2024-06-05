04ce6539aa93c7515bfbd2080bc0f63f2add5cf2
morzecho <martyna.orzechowska.stud@pw.edu.pl>
2024-06-05 17:16:23 +0200

    Build is working, changed name of file calendar

---
+++ calendar.h
@@ -0,0 +1,132 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <iomanip>
+#include <ctime>
+
+// Helper function o create std::tm from year, month, day, hour, and minute
+std::tm make_tm(int year, int month, int day, int hour = 0, int minute = 0);
+
+// Helper function to convert std::tm to std::time_t
+std::time_t tm_to_time_t(const std::tm &t);
+
+// Function to get current date
+std::tm getCurrentDate();
+
+// Plan class
+class Plan
+{
+protected:
+    std::string name;
+    std::tm date;
+
+public:
+    Plan(std::string name, std::tm date)
+        : name(name), date(date) {}
+
+    std::string getName() const;
+
+    std::tm getDate() const;
+
+    void setName(const std::string &newName);
+
+    void setDate(const std::tm &newDate);
+};
+
+// Event class
+class Event : public Plan
+{
+    std::tm start;
+    std::tm end;
+    std::string location;
+    std::vector<std::string> attendees;
+
+public:
+    Event(std::string name, std::tm start, std::tm end, std::string location, std::vector<std::string> attendees)
+        : Plan(name, start), start(start), end(end), location(location), attendees(attendees) {}
+
+    void print() const;
+
+    std::tm getStart() const;
+    void setStart(const std::tm &newDate);
+    std::tm getEnd() const;
+    void setEnd(const std::tm &newDate);
+    std::string getLocation() const;
+    void setLocation(const std::string &newLoc);
+    std::vector<std::string> getAttendees() const;
+    void setAttendees(const std::vector<std::string> &newAttendees);
+};
+
+// Task class
+class Task : public Plan
+{
+public:
+    enum Status
+    {
+        PENDING,
+        COMPLETED
+    };
+    Task(std::string name, std::tm date, bool important, std::string progressNote, Status progressStatus)
+        : Plan(name, date), important(important), progressNote(progressNote), progressStatus(progressStatus) {}
+
+    void print() const;
+
+    bool getImportant() const;
+    void setImportant(const bool &newImp);
+    std::string getProgressNote() const;
+    void setProgressNote(const std::string &newProg);
+    std::vector<Task> getSubtasks() const;
+    void addToSubtask(const Task &newSub);
+    Status getProgressStatus() const;
+    void setStatus(const Status &newStatus);
+
+private:
+    bool important;
+    std::string progressNote;
+    Status progressStatus;
+    std::vector<Task> subtasks;
+};
+
+// Calendar class
+class Calendar
+{
+public:
+    std::vector<Event> events;
+    std::vector<Task> tasks;
+
+    void addEvent(const Event &event);
+
+    void addTask(const Task &task);
+
+    std::vector<Event> filterEvents(const std::tm &start, const std::tm &end) const;
+
+    std::vector<Task> filterTasks(const std::tm &start, const std::tm &end) const;
+};
+
+// Interface class
+class Interface
+{
+public:
+    void displayCalendar(const Calendar &calendar);
+
+    void displayEventsInRange(const Calendar &calendar, const std::tm &start, const std::tm &end);
+
+    void displayTasksInRange(const Calendar &calendar, const std::tm &start, const std::tm &end);
+
+    void selectMonthAndDisplayEvents(Calendar &calendar);
+
+    void editEvent(Calendar &calendar);
+
+    void editTask(Calendar &calendar);
+
+    void addEvent(Calendar &calendar);
+
+    void addTask(Calendar &calendar);
+};
+
+// Function to get start and end date of the current week
+void getCurrentWeekRange(std::tm &weekStart, std::tm &weekEnd);
+
+// Function to get start and end date of the current month
+void getCurrentMonthRange(std::tm &monthStart, std::tm &monthEnd);
--- main.cpp
+++ main.cpp
@@ -2,16 +2,18 @@
 #include <limits>
 #include <stdexcept>
 #include <ctime>
-#include "calendar_v_3.h"
+#include "calendar.h"
 
-int main() {
+int main()
+{
     Calendar calendar;
     Interface interface;
 
     std::cout << "Welcome back\n";
 
     int choice;
-    while (true) {
+    while (true)
+    {
         std::cout << "\nSelect an operation:\n";
         std::cout << "1 - Display today's events\n";
         std::cout << "2 - Display events of the current week\n";
@@ -29,8 +31,9 @@ int main() {
         std::cin >> choice;
         std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
-        if (std::cin.fail()) {
-            std::cin.clear(); // clear error state
+        if (std::cin.fail())
+        {
+            std::cin.clear();                                                   // clear error state
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore remaining input
             std::cout << "Invalid input. Please enter a number between 1 and 12.\n";
             continue;
@@ -45,59 +48,63 @@ int main() {
         std::tm weekStart, weekEnd;
         std::tm monthStart, monthEnd;
 
-        try {
-            switch (choice) {
-                case 1:
-                    std::cout << "\nEvents for today:\n";
-                    interface.displayEventsInRange(calendar, today, endOfDay);
-                    break;
-                case 2:
-                    getCurrentWeekRange(weekStart, weekEnd);
-                    std::cout << "\nEvents for the current week:\n";
-                    interface.displayEventsInRange(calendar, weekStart, weekEnd);
-                    break;
-                case 3:
-                    getCurrentMonthRange(monthStart, monthEnd);
-                    std::cout << "\nEvents for the current month:\n";
-                    interface.displayEventsInRange(calendar, monthStart, monthEnd);
-                    break;
-                case 4:
-                    interface.selectMonthAndDisplayEvents(calendar);
-                    break;
-                case 5:
-                    interface.addEvent(calendar);
-                    break;
-                case 6:
-                    std::cout << "\nTasks for today:\n";
-                    interface.displayTasksInRange(calendar, today, endOfDay);
-                    break;
-                case 7:
-                    getCurrentWeekRange(weekStart, weekEnd);
-                    std::cout << "\nTasks for the current week:\n";
-                    interface.displayTasksInRange(calendar, weekStart, weekEnd);
-                    break;
-                case 8:
-                    getCurrentMonthRange(monthStart, monthEnd);
-                    std::cout << "\nTasks for the current month:\n";
-                    interface.displayTasksInRange(calendar, monthStart, monthEnd);
-                    break;
-                case 9:
-                    interface.addTask(calendar);
-                    break;
-                case 10:
-                    interface.editEvent(calendar);
-                    break;
-                case 11:
-                    interface.editTask(calendar);
-                    break;
-                case 12:
-                    std::cout << "Goodbye!\n";
-                    return 0;
-                default:
-                    std::cout << "Invalid choice. Please try again.\n";
-                    break;
+        try
+        {
+            switch (choice)
+            {
+            case 1:
+                std::cout << "\nEvents for today:\n";
+                interface.displayEventsInRange(calendar, today, endOfDay);
+                break;
+            case 2:
+                getCurrentWeekRange(weekStart, weekEnd);
+                std::cout << "\nEvents for the current week:\n";
+                interface.displayEventsInRange(calendar, weekStart, weekEnd);
+                break;
+            case 3:
+                getCurrentMonthRange(monthStart, monthEnd);
+                std::cout << "\nEvents for the current month:\n";
+                interface.displayEventsInRange(calendar, monthStart, monthEnd);
+                break;
+            case 4:
+                interface.selectMonthAndDisplayEvents(calendar);
+                break;
+            case 5:
+                interface.addEvent(calendar);
+                break;
+            case 6:
+                std::cout << "\nTasks for today:\n";
+                interface.displayTasksInRange(calendar, today, endOfDay);
+                break;
+            case 7:
+                getCurrentWeekRange(weekStart, weekEnd);
+                std::cout << "\nTasks for the current week:\n";
+                interface.displayTasksInRange(calendar, weekStart, weekEnd);
+                break;
+            case 8:
+                getCurrentMonthRange(monthStart, monthEnd);
+                std::cout << "\nTasks for the current month:\n";
+                interface.displayTasksInRange(calendar, monthStart, monthEnd);
+                break;
+            case 9:
+                interface.addTask(calendar);
+                break;
+            case 10:
+                interface.editEvent(calendar);
+                break;
+            case 11:
+                interface.editTask(calendar);
+                break;
+            case 12:
+                std::cout << "Goodbye!\n";
+                return 0;
+            default:
+                std::cout << "Invalid choice. Please try again.\n";
+                break;
             }
-        } catch (const std::exception& e) {
+        }
+        catch (const std::exception &e)
+        {
             std::cout << "Error: " << e.what() << "\n";
             // Clear cin state if an exception occurs
             std::cin.clear();
