7a799e7af0debeacb3f72ab3a350126d97a411c2
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2024-06-06 10:23:32 +0200

    Added function

--- event.cpp
+++ event.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <ctime>
 #include <iostream>
+#include <fstream>
 
 void Event::print() const
 {
@@ -19,6 +20,65 @@ void Event::print() const
         std::cout << attendee << " ";
     }
     std::cout << std::endl;
+}
+
+void Event::saveToFile(std::ofstream& outFile) const {
+    outFile << "Event Name: " << name << "\n";
+    outFile << "Start Date: " << std::put_time(&start, "%Y-%m-%d %H:%M") << "\n";
+    outFile << "End Date: " << std::put_time(&end, "%Y-%m-%d %H:%M") << "\n";
+    outFile << "Location: " << location << "\n";
+    outFile << "Attendees: ";
+    for (const auto& attendee : attendees) {
+        outFile << attendee << ",";
+    }
+    outFile << "\n";
+}
+
+bool Event::loadFromFile(std::ifstream& inFile, Event& event)
+{
+    std::string line;
+    if (!std::getline(inFile, line) || !startsWith(line, "Event Name:")) {
+        return false;
+    }
+
+    std::string name, startDateStr, endDateStr, location, attendeesStr;
+    std::vector<std::string> attendees;
+
+    name = line.substr(11);
+    std::getline(inFile, line);
+    startDateStr = line.substr(12);
+    std::getline(inFile, line);
+    endDateStr = line.substr(10);
+    std::getline(inFile, line);
+    location = line.substr(10);
+
+    int startYear, startMonth, startDay, startHour, startMinute;
+    std::sscanf(startDateStr.c_str(), "%d-%d-%d %d:%d", &startYear, &startMonth, &startDay, &startHour, &startMinute);
+    std::tm startDate = make_tm(startYear, startMonth, startDay, startHour, startMinute);
+
+    // Parse end date
+    int endYear, endMonth, endDay, endHour, endMinute;
+    std::sscanf(endDateStr.c_str(), "%d-%d-%d %d:%d", &endYear, &endMonth, &endDay, &endHour, &endMinute);
+    std::tm endDate = make_tm(endYear, endMonth, endDay, endHour, endMinute);
+
+    std::getline(inFile, line);
+    attendeesStr = line.substr(11);
+    if (attendeesStr.empty()) {
+        event = Event(name, startDate, endDate, location, attendees);
+        return true;
+    }
+    std::cout << attendeesStr;
+    std::stringstream attendeesStream(attendeesStr);
+    std::string attendee;
+    while (attendeesStream.good()) {
+        std::string substr;
+        getline(attendeesStream, substr, ',');
+        attendees.push_back(substr);
+    }
+
+    event = Event(name, startDate, endDate, location, attendees);
+    return true;
+
 }
 
 std::tm Event::getStart() const
