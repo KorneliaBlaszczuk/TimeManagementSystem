87d21700ddd2436024efe69e533068aba63fbad6
KorneliaBlaszczuk <kornelia.blaszczuk@onet.pl>
2024-06-06 22:35:00 +0200

    Upload New File

---
+++ interface.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "calendar.h"
+#include "event.h"
+#include "task.h"
+#include "reminder.h"
+#include "pomodoro.h"
+#include "plan.h"
+#include "functions.h"
+
+// Interface class
+class Interface
+{
+public:
+    int openFile(Calendar &calendar, std::string eventF, std::string tasksF, std::string completedT);
+
+    void displayCalendar(const Calendar &calendar);
+
+    void displayEventsInRange(const Calendar &calendar, const std::tm &start, const std::tm &end);
+
+    void displayTasksInRange(const Calendar &calendar, const std::tm &start, const std::tm &end);
+
+    void displayTasksCompleted(const Calendar &calendar);
+
+    // TODO displaying reminders
+    void displayTodayReminders(const Calendar &calendar);
+
+    void displayTommorowReminders(const Calendar &calendar);
+
+    void selectMonthAndDisplayEvents(Calendar &calendar);
+
+    void editEvent(Calendar &calendar, std::string file);
+
+    void editTask(Calendar &calendar, std::string fileTasks, std::string fileCompleted);
+
+    void editReminder(Calendar &Calendar, std::string reminderFile, std::string completedFile);
+
+    void addReminder(Calendar &Calendar, std::string reminderFile, std::string completedFile);
+
+    void addEvent(Calendar &calendar, std::string file);
+
+    void addTask(Calendar &calendar, std::string fileTasks, std::string fileCompleted);
+
+    // TODO - all delete functions
+
+    void deleteTask(Calendar &calendar, std::string taskFile);
+
+    void deleteEvent(Calendar &calendar, std::string outFile);
+
+    void deleteReminder(Calendar &calendar, std::string reminderFile);
+
+    // function use to run predefined pomodoro mode in project
+    void PomodoroRun();
+};
