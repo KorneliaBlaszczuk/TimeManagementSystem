3b92c1aae4387b0730ad9abfb3867fb4591eb779
morzecho <martyna.orzechowska.stud@pw.edu.pl>
2024-06-07 09:45:08 +0200

    Added comments in plan

--- plan.h
+++ plan.h
@@ -7,22 +7,32 @@
 
 // Plan class
 class Plan
+/*
+Base class for all classes consisting of planning in projects. It has name and data as an attributes.
+*/
 {
 protected:
     std::string name;
     std::tm date;
 
 public:
+    // constructor of plan object
     Plan(std::string const &name, std::tm const &date)
         : name(name), date(date) {}
 
+    // Getting name of the plan object.
     std::string getName() const;
 
+    // Getting date in tm format of the plan object.
     std::tm getDate() const;
 
+    // Setting new name to plan object.
     void setName(const std::string &newName);
 
+    // Setting new date (in tm format) to object plan.
     void setDate(const std::tm &newDate);
 
-    static bool startsWith(const std::string& line, const std::string& prefix);
+    // Checks if the line has the same prefix as given.
+    //@TOCHECK - czy to ma byc w tej klasie? Moze pomocnicze funkcje?
+    static bool startsWith(const std::string &line, const std::string &prefix);
 };
